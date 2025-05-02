7e68f14fa1ecd6b52c7f8ef7fb59d09a01d10c37
yash49766 <yashkapadiya191@gmail.com>
2025-05-02 16:18:18 +0530

    Contact Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import './App.css'
 import Navbar from "./components/global/navbar.jsx";
-import {Routes , Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Home from "./pages/home.jsx";
 import Ourprocess from "./pages/ourprocess.jsx";
 import About from "./pages/about.jsx";
+import Contactus from "./pages/contactus.jsx";
 
 function App() {
 
-  return (
-    <>
-<Navbar />
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/process" element={<Ourprocess/>}/>
-            <Route path="/about" element={<About/>}/>
-        </Routes>
-        {/*<Footer />*/}
-    </>
-  )
+    return (
+        <>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/process" element={<Ourprocess/>}/>
+                <Route path="/contact" element={<Contactus/>}/>
+                <Route path="/about" element={<About/>}/>
+            </Routes>
+            {/*<Footer />*/}
+        </>
+    )
 }
 
 export default App
