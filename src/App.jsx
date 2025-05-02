121a77b83c44ca6cd086105b5cf3144c20f017ca
yash49766 <yashkapadiya191@gmail.com>
2025-05-02 09:43:49 +0530

    Home Page changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/global/navbar.jsx";
 import Footer from "./components/global/footer.jsx";
 import Productss from "./pages/homepage/productss.jsx";
 import About from "./pages/homepage/about.jsx";
+import Milestonee from "./pages/homepage/milestonee.jsx";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
 <Navbar />
         <Productss/>
         <About/>
+        <Milestonee/>
         {/*<Router>*/}
         {/*    <Routes>*/}
         {/*        <Route path="/products" element={<Products />} />*/}
---
+++ src/pages/homepage/milestonee.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Milestone from "../../components/homePage/milestone.jsx";
+
+function Milestonee() {
+    return (
+        <>
+        <Milestone />
+        </>
+    );
+}
+
+export default Milestonee;
