64670bfc0d282ae7e90bcf3adc3bbef0bb55e2ad
gpatel11 <accoubntt@gmail.com>
2025-05-03 14:38:56 +0530

    changes in about us

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import OurVision from "../components/aboutUs/ourVision.jsx";
+import WhoWeAre from "../components/aboutUs/whoweare.jsx";
 
 function About() {
     return (
         <>
             <OurVision />
+            <WhoWeAre/>
         </>
     );
 }
